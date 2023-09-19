27dec2077d8a41268ecc84eb2dfce628eb9c85bb
mohd2126 <41587665+mohd2126@users.noreply.github.com>
2023-09-19 14:51:41 +0300

    Update data_bonusaction.js

--- crobi-dnd5e-quickref-edited-with-homebrew/js/data_bonusaction.js
+++ crobi-dnd5e-quickref-edited-with-homebrew/js/data_bonusaction.js
@@ -1,18 +1,5 @@
 data_bonusaction = [
     {
-        title: "Offhand Attack",
-        icon: "crossed-swords",
-        subtitle: "Use with the Attack action",
-        description: "Attack with your off hand",
-        reference: "PHB, pgs. 192,194-195.",
-        bullets: [
-            "Only usable if you take the <i>Attack</i> action and attack with a light melee weapon that you're holding in one hand.",
-            "Perform a single attack with a different light melee weapon that you're holding in the other hand.",
-            "You don't add your ability modifier to the damage of the bonus attack, unless that modifier is negative.",
-            "If either weapon has the thrown property, you can throw the weapon, instead of making a melee attack with it."
-        ]
-    },
-    {
         title: "Cast a spell",
         icon: "magic-swirl",
         subtitle: "Cast time of 1 bonus action",
