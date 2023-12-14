b06200cf1810fee51e5a16e7e98682f6a754860c
mohd2126 <41587665+mohd2126@users.noreply.github.com>
2023-12-14 15:28:12 +0300

    Update data_bonusaction.js
    Added Tumble and Overrun

--- crobi-dnd5e-quickref-edited-with-homebrew/js/data_bonusaction.js
+++ crobi-dnd5e-quickref-edited-with-homebrew/js/data_bonusaction.js
@@ -6,7 +6,7 @@ data_bonusaction = [
         description: "Cast a spell with a casting time of 1 bonus action",
         reference: "PHB, pg. 192.",
         bullets: [
-            "You can't cast a spell with you action and a different spell with your bonus action in the same turn, except if the action is used to cast a cantrip.",
+            "You can't cast a spell with you action and a different spell with your bonus action in the same turn, except if the bonus action is used to cast a cantrip.",
             "For further details, see the <i>Cast a spell</i> action."
         ]
     },
@@ -18,6 +18,26 @@ data_bonusaction = [
         reference: "See class page for more information.",
         bullets: [
 
+        ]
+    },
+    {
+        title: "Overrun",
+        icon: "rhinoceros-horn",
+        subtitle: "Force your way through another creature's space",
+        description: "",
+        reference: "DMG, pg. 272.",
+        bullets: [
+            "When a creature tries to move through a hostile creature's space, the mover can try to force its way through by overrunning the hostile creature. As an action or a bonus action, the mover makes a Strength (Athletics) check contested by the hostile creature's Strength (Athletics) check. The creature attempting the overrun has advantage on this check if it is larger than the hostile creature, or disadvantage if it is smaller. If the mover wins the contest, it can move through the hostile creature's space once this turn."
+        ]
+    },
+    {
+        title: "Tumble",
+        icon: "foot-trip",
+        subtitle: "Slip through another creature's space",
+        description: "",
+        reference: "DMG, pg. 272.",
+        bullets: [
+            "A creature can try to tumble through a hostile creature's space, ducking and weaving past the opponent. As an action or a bonus action, the tumbler makes a Dexterity (Acrobatics) check contested by the hostile creature's Dexterity (Acrobatics) check. If the tumbler wins the contest, it can move through the hostile creature's space once this turn."
         ]
     }
 ]
